6c9d84670a8f804d93d5cd2b3936459570a6c2bf
Redoan Rafi <redoan1080@gmail.com>
2024-08-16 22:52:28 +0600

    cleaner look without bracelets

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,7 +2,6 @@ import { Toaster } from "sonner";
 import Accordian from "./Accordian/Accordian";
 import BanglesCards from "./BanglesCards/BanglesCards";
 import BannerSlider from "./BannerSlider/BannerSlider";
-import BraceletCards from "./BraceletCards/BraceletCards";
 
 const Home = () => {
     return (
@@ -13,12 +12,11 @@ const Home = () => {
             <div className=" mx-auto text-center">
                 <BanglesCards />
             </div>
-            <div className=" mx-auto">
-                <BraceletCards />
-            </div>
+            
             <div>
                 <Accordian />
             </div>
+            
             {/*below the div is for toaster not any section of home page */}
             <div>
                 <Toaster position="bottom-right" closeButton={true} richColors />
